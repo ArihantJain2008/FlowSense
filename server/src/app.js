71e62991343eabaf9322dd5f6b3b0aed6166b7d1
Arihant Jain <your-email@example.com>
2026-06-17 16:36:31 +0530

    added backend.(it is not working YET)

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Auth Routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "FlowSense API Running",
+  });
+});
+
+module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,15 @@
-const express = require("express");
-const cors = require("cors");
+require("dotenv").config();
 
-const app = express();
+const app = require("./app");
+const connectDB = require("./config/db");
+console.log("URI loaded:", process.env.MONGO_URI);
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "FlowSense API Running",
-  });
-});
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(
+    `Server running on port ${PORT}`
+  );
 });
