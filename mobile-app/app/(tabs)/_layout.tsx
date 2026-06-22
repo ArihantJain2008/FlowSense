deb788921a0ea717cfa0f22a466f35a7c9712315
Arihant Jain <your-email@example.com>
2026-06-22 11:20:36 +0530

    added profile option

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -109,6 +109,20 @@ export default function TabsLayout() {
           ),
         }}
       />
+
+      <Tabs.Screen
+  name="profile"
+  options={{
+    title: "Profile",
+    tabBarIcon: ({ color, size }) => (
+      <Ionicons
+        name="person-circle"
+        size={size}
+        color={color}
+      />
+    ),
+  }}
+/>
     </Tabs>
   );
 }
